82a284a6df8d468476ba2096aa163f48921bdc47
Karol Gurba <karolgurba22@gmail.com>
2018-09-11 18:50:15 -0500

    cleaned up the code

--- main.py
+++ main.py
@@ -4,36 +4,43 @@ import turtle
 import time
 
 def find_iss():
+  #url for astronauts in space
   url = 'http://api.open-notify.org/astros.json'
-  response = urllib.request.urlopen(url)
-  results = json.loads(response.read())
+  #url for current location of iss
+  iss_url = 'http://api.open-notify.org/iss-now.json'
+  #url for overhead times in Chicago
+  iss_ovh_url = 'http://api.open-notify.org/iss-pass.json?lat=41.878113&lon=-87.629799'
 
-  astros = results['people']
+  #get list of astronauts in space
+  iss_astros = json_results(url)
+  astros = iss_astros['people']
 
-  print('People in space:', results['number'])
-
+  #prints out how many astros are currenlty in space
+  print('People in space:', iss_astros['number'])
+  #prints out each atsor name and what craft they are in
   for a in astros:
     print(a['name'], 'is in', a['craft'])
 
-  iss_url = 'http://api.open-notify.org/iss-now.json'
-  iss_response = urllib.request.urlopen(iss_url)
-  iss_results = json.loads(iss_response.read())
-
-  iss_lat = float(iss_results['iss_position']['latitude'])
-  iss_long = float(iss_results['iss_position']['longitude'])
-
+  #screen setup
   screen = turtle.Screen()
   screen.setup(720,360)
   screen.title('ISS-Locator')
   screen.setworldcoordinates(-180, -90, 180, 90)
   screen.bgpic('images/map.gif')
 
+  #assings current ISS lat and long to variables to be used later
+  iss_pos = json_results(iss_url)
+  iss_lat = float(iss_pos['iss_position']['latitude'])
+  iss_long = float(iss_pos['iss_position']['longitude'])
+
+  #sets a turtle at current ISS position
   screen.register_shape('images/iss.gif')
   iss = turtle.Turtle()
   iss.shape('images/iss.gif')
   iss.penup()
   iss.goto(iss_long, iss_lat)
 
+  #makes a dot at city of choice, Chicago atm
   location = turtle.Turtle()
   location.penup()
   location.color('red')
@@ -41,14 +48,26 @@ def find_iss():
   location.dot(5)
   location.hideturtle()
 
-  url = 'http://api.open-notify.org/iss-pass.json?lat=41.881832&lon=-87.623177'
-  loc_response = urllib.request.urlopen(url)
-  loc_results = json.loads(loc_response.read())
-
-  over_head_time = loc_results['response'][0]['risetime']
+  #prints out the next time the ISS will be over a city
+  iss_ovh = json_results(iss_ovh_url)
+  over_head_time = iss_ovh['response'][0]['risetime']
   location.write(time.ctime(over_head_time), font=6)
 
 
   screen.exitonclick()
 
+
+def json_results(url):
+  response = urllib.request.urlopen(url)
+  results = json.loads(response.read())
+  return results
+
+
 find_iss()
+
+
+
+
+
+
+
